86a28a1d41377ca85d01870a22c248239bee4bad
cinnamon-rolling <jyng.9922@gmail.com>
2019-11-26 20:21:42 +0800

    feat: added bulls and cows

--- work/planAhead/game/game.srcs/sources_1/imports/verilog/game_FSM_6.v
+++ work/planAhead/game/game.srcs/sources_1/imports/verilog/game_FSM_6.v
@@ -20,49 +20,62 @@ module game_FSM_6 (
   
   
   
-  localparam START_state = 5'd0;
-  localparam IDLE_state = 5'd1;
-  localparam STATE_RED_state = 5'd2;
-  localparam STATE_GREEN_state = 5'd3;
-  localparam STATE_BLUE_state = 5'd4;
-  localparam CHECK_STATE_state = 5'd5;
-  localparam CHECK_SUCCESS_state = 5'd6;
-  localparam CHECK_BULL_INIT_state = 5'd7;
-  localparam CHECK_BULL_0_state = 5'd8;
-  localparam CHECK_BULL_1_state = 5'd9;
-  localparam CHECK_BULL_2_state = 5'd10;
-  localparam CHECK_BULL_3_state = 5'd11;
-  localparam CHECK_COW_state = 5'd12;
-  localparam CHECK_LIFE_PART1_state = 5'd13;
-  localparam CHECK_LIFE_PART2_state = 5'd14;
-  localparam CHECK_BULL_COW_state = 5'd15;
-  localparam SUCCESS_state = 5'd16;
-  localparam FAIL_state = 5'd17;
+  localparam START_state = 6'd0;
+  localparam IDLE_state = 6'd1;
+  localparam STATE_RED_state = 6'd2;
+  localparam STATE_GREEN_state = 6'd3;
+  localparam STATE_BLUE_state = 6'd4;
+  localparam CHECK_STATE_state = 6'd5;
+  localparam CHECK_SUCCESS_state = 6'd6;
+  localparam CHECK_BULL_INIT_state = 6'd7;
+  localparam CHECK_BULL_0_state = 6'd8;
+  localparam CHECK_BULL_1_state = 6'd9;
+  localparam CHECK_BULL_2_state = 6'd10;
+  localparam CHECK_BULL_3_state = 6'd11;
+  localparam CHECK_COW_INIT_state = 6'd12;
+  localparam CHECK_COW_0_0_state = 6'd13;
+  localparam CHECK_COW_0_1_state = 6'd14;
+  localparam CHECK_COW_0_2_state = 6'd15;
+  localparam CHECK_COW_0_3_state = 6'd16;
+  localparam CHECK_COW_1_0_state = 6'd17;
+  localparam CHECK_COW_1_1_state = 6'd18;
+  localparam CHECK_COW_1_2_state = 6'd19;
+  localparam CHECK_COW_1_3_state = 6'd20;
+  localparam CHECK_COW_2_0_state = 6'd21;
+  localparam CHECK_COW_2_1_state = 6'd22;
+  localparam CHECK_COW_2_2_state = 6'd23;
+  localparam CHECK_COW_2_3_state = 6'd24;
+  localparam CHECK_COW_3_0_state = 6'd25;
+  localparam CHECK_COW_3_1_state = 6'd26;
+  localparam CHECK_COW_3_2_state = 6'd27;
+  localparam CHECK_COW_3_3_state = 6'd28;
+  localparam CHECK_LIFE_PART1_state = 6'd29;
+  localparam CHECK_LIFE_PART2_state = 6'd30;
+  localparam CHECK_BULL_COW_state = 6'd31;
+  localparam SUCCESS_state = 6'd32;
+  localparam FAIL_state = 6'd33;
   
-  reg [4:0] M_state_d, M_state_q = START_state;
+  reg [5:0] M_state_d, M_state_q = START_state;
   reg [2:0] M_check_state_d, M_check_state_q = 1'h0;
   reg [15:0] M_user_input_d, M_user_input_q = 1'h0;
-  reg [15:0] M_user_input_cow_d, M_user_input_cow_q = 1'h0;
-  reg [15:0] M_output_cow_d, M_output_cow_q = 1'h0;
-  reg [15:0] M_output_bull_d, M_output_bull_q = 1'h0;
-  reg [2:0] M_k_d, M_k_q = 1'h0;
-  reg [15:0] M_temp_ans_d, M_temp_ans_q = 1'h0;
-  reg [15:0] M_temp_input_d, M_temp_input_q = 1'h0;
+  reg [15:0] M_temp_ans_bull_d, M_temp_ans_bull_q = 1'h0;
+  reg [15:0] M_temp_input_bull_d, M_temp_input_bull_q = 1'h0;
   reg [2:0] M_bull_count_d, M_bull_count_q = 1'h0;
-  
-  reg [2:0] end_index;
-  
-  reg [2:0] start_index;
+  reg [15:0] M_temp_ans_cow_d, M_temp_ans_cow_q = 1'h0;
+  reg [15:0] M_temp_input_cow_d, M_temp_input_cow_q = 1'h0;
+  reg [2:0] M_cow_count_d, M_cow_count_q = 1'h0;
   
   reg [15:0] answer;
   
   always @* begin
     M_state_d = M_state_q;
+    M_temp_input_bull_d = M_temp_input_bull_q;
     M_bull_count_d = M_bull_count_q;
-    M_temp_ans_d = M_temp_ans_q;
+    M_cow_count_d = M_cow_count_q;
     M_user_input_d = M_user_input_q;
-    M_temp_input_d = M_temp_input_q;
-    M_k_d = M_k_q;
+    M_temp_ans_cow_d = M_temp_ans_cow_q;
+    M_temp_input_cow_d = M_temp_input_cow_q;
+    M_temp_ans_bull_d = M_temp_ans_bull_q;
     M_check_state_d = M_check_state_q;
     
     alufn = 1'h0;
@@ -71,8 +84,8 @@ module game_FSM_6 (
     demux = 1'h0;
     led_out = 1'h0;
     led_out_1 = M_bull_count_q;
-    led_out_2 = 1'h0;
-    answer = 16'h9999;
+    led_out_2 = M_cow_count_q;
+    answer = 16'h3999;
     
     case (M_state_q)
       START_state: begin
@@ -86,22 +99,22 @@ module game_FSM_6 (
         end
       end
       STATE_RED_state: begin
-        led_out = 8'h09;
+        led_out = 4'h9;
         if (toggle) begin
           M_state_d = STATE_GREEN_state;
         end else begin
           if (enter) begin
             if (M_check_state_q == 1'h0) begin
-              M_user_input_d[0+3-:4] = 4'h9;
+              M_user_input_d[12+3-:4] = 4'h9;
             end else begin
               if (M_check_state_q == 1'h1) begin
-                M_user_input_d[4+3-:4] = 4'h9;
+                M_user_input_d[8+3-:4] = 4'h9;
               end else begin
                 if (M_check_state_q == 2'h2) begin
-                  M_user_input_d[8+3-:4] = 4'h9;
+                  M_user_input_d[4+3-:4] = 4'h9;
                 end else begin
                   if (M_check_state_q == 2'h3) begin
-                    M_user_input_d[12+3-:4] = 4'h9;
+                    M_user_input_d[0+3-:4] = 4'h9;
                   end
                 end
               end
@@ -112,22 +125,22 @@ module game_FSM_6 (
         end
       end
       STATE_GREEN_state: begin
-        led_out = 8'h05;
+        led_out = 4'h5;
         if (toggle) begin
           M_state_d = STATE_BLUE_state;
         end else begin
           if (enter) begin
             if (M_check_state_q == 1'h0) begin
-              M_user_input_d[0+3-:4] = 4'h5;
+              M_user_input_d[12+3-:4] = 4'h5;
             end else begin
               if (M_check_state_q == 1'h1) begin
-                M_user_input_d[4+3-:4] = 4'h5;
+                M_user_input_d[8+3-:4] = 4'h5;
               end else begin
                 if (M_check_state_q == 2'h2) begin
-                  M_user_input_d[8+3-:4] = 4'h5;
+                  M_user_input_d[4+3-:4] = 4'h5;
                 end else begin
                   if (M_check_state_q == 2'h3) begin
-                    M_user_input_d[12+3-:4] = 4'h5;
+                    M_user_input_d[0+3-:4] = 4'h5;
                   end
                 end
               end
@@ -138,22 +151,22 @@ module game_FSM_6 (
         end
       end
       STATE_BLUE_state: begin
-        led_out = 8'h0d;
+        led_out = 4'h3;
         if (toggle) begin
           M_state_d = STATE_RED_state;
         end else begin
           if (enter) begin
             if (M_check_state_q == 1'h0) begin
-              M_user_input_d[0+3-:4] = 4'h3;
+              M_user_input_d[12+3-:4] = 4'h3;
             end else begin
               if (M_check_state_q == 1'h1) begin
-                M_user_input_d[4+3-:4] = 4'h3;
+                M_user_input_d[8+3-:4] = 4'h3;
               end else begin
                 if (M_check_state_q == 2'h2) begin
-                  M_user_input_d[8+3-:4] = 4'h3;
+                  M_user_input_d[4+3-:4] = 4'h3;
                 end else begin
                   if (M_check_state_q == 2'h3) begin
-                    M_user_input_d[12+3-:4] = 4'h3;
+                    M_user_input_d[0+3-:4] = 4'h3;
                   end
                 end
               end
@@ -174,45 +187,178 @@ module game_FSM_6 (
         M_state_d = CHECK_BULL_INIT_state;
       end
       CHECK_BULL_INIT_state: begin
-        M_k_d = 1'h0;
-        M_temp_ans_d = answer;
-        M_temp_input_d = M_user_input_q;
+        M_temp_ans_bull_d = answer;
+        M_temp_input_bull_d = M_user_input_q;
         M_bull_count_d = 1'h0;
-        M_state_d = CHECK_BULL_1_state;
+        M_state_d = CHECK_BULL_0_state;
       end
       CHECK_BULL_0_state: begin
-        if (M_temp_ans_q[0+3-:4] == M_temp_input_q[0+3-:4]) begin
+        if (M_temp_ans_bull_q[0+3-:4] == M_temp_input_bull_q[0+3-:4]) begin
           M_bull_count_d = M_bull_count_q + 1'h1;
-          M_state_d = CHECK_BULL_1_state;
+          M_temp_input_cow_d[0+3-:4] = 4'h0;
         end else begin
-          M_state_d = CHECK_BULL_1_state;
+          M_temp_input_cow_d[0+3-:4] = M_temp_input_bull_q[0+3-:4];
         end
-        M_k_d = M_k_q + 1'h1;
+        M_state_d = CHECK_BULL_1_state;
       end
       CHECK_BULL_1_state: begin
-        if (M_temp_ans_q[4+3-:4] == M_temp_input_q[4+3-:4]) begin
+        if (M_temp_ans_bull_q[4+3-:4] == M_temp_input_bull_q[4+3-:4]) begin
           M_bull_count_d = M_bull_count_q + 1'h1;
-          M_state_d = CHECK_BULL_2_state;
+          M_temp_input_cow_d[4+3-:4] = 4'h0;
         end else begin
-          M_state_d = CHECK_BULL_2_state;
+          M_temp_input_cow_d[4+3-:4] = M_temp_input_bull_q[4+3-:4];
         end
-        M_k_d = M_k_q + 1'h1;
+        M_state_d = CHECK_BULL_2_state;
       end
       CHECK_BULL_2_state: begin
-        if (M_temp_ans_q[8+3-:4] == M_temp_input_q[8+3-:4]) begin
+        if (M_temp_ans_bull_q[8+3-:4] == M_temp_input_bull_q[8+3-:4]) begin
           M_bull_count_d = M_bull_count_q + 1'h1;
-          M_state_d = CHECK_BULL_3_state;
+          M_temp_input_cow_d[8+3-:4] = 4'h0;
         end else begin
-          M_state_d = CHECK_BULL_3_state;
+          M_temp_input_cow_d[8+3-:4] = M_temp_input_bull_q[8+3-:4];
         end
-        M_k_d = M_k_q + 1'h1;
+        M_state_d = CHECK_BULL_3_state;
       end
       CHECK_BULL_3_state: begin
-        if (M_temp_ans_q[12+3-:4] == M_temp_input_q[12+3-:4]) begin
+        if (M_temp_ans_bull_q[12+3-:4] == M_temp_input_bull_q[12+3-:4]) begin
           M_bull_count_d = M_bull_count_q + 1'h1;
-          M_state_d = STATE_RED_state;
+          M_temp_input_cow_d[12+3-:4] = 4'h0;
         end else begin
-          M_state_d = STATE_GREEN_state;
+          M_temp_input_cow_d[12+3-:4] = M_temp_input_bull_q[12+3-:4];
+        end
+        M_state_d = CHECK_COW_INIT_state;
+      end
+      CHECK_COW_INIT_state: begin
+        M_temp_ans_cow_d = answer;
+        M_cow_count_d = 1'h0;
+        M_state_d = CHECK_COW_0_0_state;
+      end
+      CHECK_COW_0_0_state: begin
+        if (M_temp_input_cow_q[0+3-:4] == M_temp_ans_cow_q[0+3-:4]) begin
+          M_cow_count_d = M_cow_count_q + 1'h1;
+          M_state_d = CHECK_COW_1_0_state;
+        end else begin
+          M_state_d = CHECK_COW_0_1_state;
+        end
+      end
+      CHECK_COW_0_1_state: begin
+        if (M_temp_input_cow_q[0+3-:4] == M_temp_ans_cow_q[4+3-:4]) begin
+          M_cow_count_d = M_cow_count_q + 1'h1;
+          M_state_d = CHECK_COW_1_0_state;
+        end else begin
+          M_state_d = CHECK_COW_0_2_state;
+        end
+      end
+      CHECK_COW_0_2_state: begin
+        if (M_temp_input_cow_q[0+3-:4] == M_temp_ans_cow_q[8+3-:4]) begin
+          M_cow_count_d = M_cow_count_q + 1'h1;
+          M_state_d = CHECK_COW_1_0_state;
+        end else begin
+          M_state_d = CHECK_COW_0_3_state;
+        end
+      end
+      CHECK_COW_0_3_state: begin
+        if (M_temp_input_cow_q[0+3-:4] == M_temp_ans_cow_q[12+3-:4]) begin
+          M_cow_count_d = M_cow_count_q + 1'h1;
+          M_state_d = CHECK_COW_1_0_state;
+        end else begin
+          M_state_d = CHECK_COW_1_0_state;
+        end
+      end
+      CHECK_COW_1_0_state: begin
+        if (M_temp_input_cow_q[4+3-:4] == M_temp_ans_cow_q[0+3-:4]) begin
+          M_cow_count_d = M_cow_count_q + 1'h1;
+          M_state_d = CHECK_COW_2_0_state;
+        end else begin
+          M_state_d = CHECK_COW_1_1_state;
+        end
+      end
+      CHECK_COW_1_1_state: begin
+        if (M_temp_input_cow_q[4+3-:4] == M_temp_ans_cow_q[4+3-:4]) begin
+          M_cow_count_d = M_cow_count_q + 1'h1;
+          M_state_d = CHECK_COW_2_0_state;
+        end else begin
+          M_state_d = CHECK_COW_1_2_state;
+        end
+      end
+      CHECK_COW_1_2_state: begin
+        if (M_temp_input_cow_q[4+3-:4] == M_temp_ans_cow_q[8+3-:4]) begin
+          M_cow_count_d = M_cow_count_q + 1'h1;
+          M_state_d = CHECK_COW_2_0_state;
+        end else begin
+          M_state_d = CHECK_COW_1_3_state;
+        end
+      end
+      CHECK_COW_1_3_state: begin
+        if (M_temp_input_cow_q[4+3-:4] == M_temp_ans_cow_q[12+3-:4]) begin
+          M_cow_count_d = M_cow_count_q + 1'h1;
+          M_state_d = CHECK_COW_2_0_state;
+        end else begin
+          M_state_d = CHECK_COW_2_0_state;
+        end
+      end
+      CHECK_COW_2_0_state: begin
+        if (M_temp_input_cow_q[8+3-:4] == M_temp_ans_cow_q[0+3-:4]) begin
+          M_cow_count_d = M_cow_count_q + 1'h1;
+          M_state_d = CHECK_COW_3_0_state;
+        end else begin
+          M_state_d = CHECK_COW_2_1_state;
+        end
+      end
+      CHECK_COW_2_1_state: begin
+        if (M_temp_input_cow_q[8+3-:4] == M_temp_ans_cow_q[4+3-:4]) begin
+          M_cow_count_d = M_cow_count_q + 1'h1;
+          M_state_d = CHECK_COW_3_0_state;
+        end else begin
+          M_state_d = CHECK_COW_2_2_state;
+        end
+      end
+      CHECK_COW_2_2_state: begin
+        if (M_temp_input_cow_q[8+3-:4] == M_temp_ans_cow_q[8+3-:4]) begin
+          M_cow_count_d = M_cow_count_q + 1'h1;
+          M_state_d = CHECK_COW_3_0_state;
+        end else begin
+          M_state_d = CHECK_COW_2_3_state;
+        end
+      end
+      CHECK_COW_2_3_state: begin
+        if (M_temp_input_cow_q[8+3-:4] == M_temp_ans_cow_q[12+3-:4]) begin
+          M_cow_count_d = M_cow_count_q + 1'h1;
+          M_state_d = CHECK_COW_3_0_state;
+        end else begin
+          M_state_d = CHECK_COW_3_0_state;
+        end
+      end
+      CHECK_COW_3_0_state: begin
+        if (M_temp_input_cow_q[12+3-:4] == M_temp_ans_cow_q[0+3-:4]) begin
+          M_cow_count_d = M_cow_count_q + 1'h1;
+          M_state_d = IDLE_state;
+        end else begin
+          M_state_d = CHECK_COW_3_1_state;
+        end
+      end
+      CHECK_COW_3_1_state: begin
+        if (M_temp_input_cow_q[12+3-:4] == M_temp_ans_cow_q[4+3-:4]) begin
+          M_cow_count_d = M_cow_count_q + 1'h1;
+          M_state_d = IDLE_state;
+        end else begin
+          M_state_d = CHECK_COW_3_2_state;
+        end
+      end
+      CHECK_COW_3_2_state: begin
+        if (M_temp_input_cow_q[12+3-:4] == M_temp_ans_cow_q[8+3-:4]) begin
+          M_cow_count_d = M_cow_count_q + 1'h1;
+          M_state_d = IDLE_state;
+        end else begin
+          M_state_d = CHECK_COW_3_3_state;
+        end
+      end
+      CHECK_COW_3_3_state: begin
+        if (M_temp_input_cow_q[12+3-:4] == M_temp_ans_cow_q[12+3-:4]) begin
+          M_cow_count_d = M_cow_count_q + 1'h1;
+          M_state_d = IDLE_state;
+        end else begin
+          M_state_d = IDLE_state;
         end
       end
     endcase
@@ -222,24 +368,22 @@ module game_FSM_6 (
     if (rst == 1'b1) begin
       M_check_state_q <= 1'h0;
       M_user_input_q <= 1'h0;
-      M_user_input_cow_q <= 1'h0;
-      M_output_cow_q <= 1'h0;
-      M_output_bull_q <= 1'h0;
-      M_k_q <= 1'h0;
-      M_temp_ans_q <= 1'h0;
-      M_temp_input_q <= 1'h0;
+      M_temp_ans_bull_q <= 1'h0;
+      M_temp_input_bull_q <= 1'h0;
       M_bull_count_q <= 1'h0;
+      M_temp_ans_cow_q <= 1'h0;
+      M_temp_input_cow_q <= 1'h0;
+      M_cow_count_q <= 1'h0;
       M_state_q <= 1'h0;
     end else begin
       M_check_state_q <= M_check_state_d;
       M_user_input_q <= M_user_input_d;
-      M_user_input_cow_q <= M_user_input_cow_d;
-      M_output_cow_q <= M_output_cow_d;
-      M_output_bull_q <= M_output_bull_d;
-      M_k_q <= M_k_d;
-      M_temp_ans_q <= M_temp_ans_d;
-      M_temp_input_q <= M_temp_input_d;
+      M_temp_ans_bull_q <= M_temp_ans_bull_d;
+      M_temp_input_bull_q <= M_temp_input_bull_d;
       M_bull_count_q <= M_bull_count_d;
+      M_temp_ans_cow_q <= M_temp_ans_cow_d;
+      M_temp_input_cow_q <= M_temp_input_cow_d;
+      M_cow_count_q <= M_cow_count_d;
       M_state_q <= M_state_d;
     end
   end
